1d87b4f0afd017caf8e3f86a025606aea1bb9583
Jeff Bai <j.bai@wustl.edu>
2020-05-17 01:21:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  queue = []
+  i = 0
+  
+  if array.length >=1
+    array.each_with_index {|person,index|
+      queue.push("#{index +1}. #{person}")}
+      puts "The line is currently: #{queue.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+  
+end
