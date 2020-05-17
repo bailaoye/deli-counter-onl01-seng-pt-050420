3ff59dd358cc137d2fd77f7da0da16db790b750a
Jeff Bai <j.bai@wustl.edu>
2020-05-17 01:22:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,17 @@ def line(array)
   end
   
 end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length > 0
+    puts "Currently serving #{array[0]}."
+    array.shift
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+end
